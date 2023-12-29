39b22a01cd70e9a520e7a26c070e97c1b6cb9039
jason <jason@hedmans.org>
2023-12-29 09:39:48 -0500

    add AI to header

--- data/profileHeader.ts
+++ data/profileHeader.ts
@@ -1,6 +1,6 @@
 const profileHeader = {
     name: 'Jason Hedman',
-    subtitle: 'Full-stack software engineer with experience in blockchain, web, gaming, mobile, fintech, and edtech development.',
+    subtitle: 'Full-stack software engineer with experience in blockchain, AI, web, gaming, mobile, fintech, and edtech development.',
     location: 'Boston, MA',
     about: 'I have been a software developer for over 10 years. I have worked on a variety of projects, from small websites to large enterprise applications. I am passionate about learning new technologies and sharing my knowledge with others.'
 } as const;
