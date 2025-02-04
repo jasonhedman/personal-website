46bff72c7503db81c226f913b5aa2da4432ba653
Jason <jason@hedmans.org>
2025-02-04 09:56:34 -0800

    edit education

--- data/educations.ts
+++ data/educations.ts
@@ -4,7 +4,7 @@ const educations: Education[] = [
     {
         school: 'Vanderbilt University',
         degree: 'B.S. in Computer Science, B.A. in Mathematics, Minor in Engineering Management',
-        description: 'Change++, Project Manager; Anchor DAO (Blockchain Club), Member; Vanderbilt Investment Club, Senior Analyst (C&R); Kappa Sigma Fraternity, Chapter President',
+        description: 'Change++, Project Manager; Anchor DAO (Blockchain Club), Member; Vanderbilt Investment Club, Senior Analyst (C&R)',
         startYear: 2020,
         endYear: 2024
     }
